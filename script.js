ba953d8402bdc1ee6b569979702ee5c1a90218f5
amoore33 <71346578+amoore33@users.noreply.github.com>
2020-09-15 14:44:24 -0400

    Fix name prompt

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var boxEle = document.body.querySelector(".box");
-
+var box = document.body.querySelector(".box");
+var box2 = document.body.querySelector(".box2");
 var userName = window.prompt("What is your name?");
 
 var x1 = 5;
 var x2 = 3;
 
-boxEle.innerHTML = x1 + x2;
-boxEle.innerHTML += userName;
+box.innerHTML = x1 + x2;
+box2.innerHTML = userName;

d0e87985a7a5e84010e53b5a2b590d07b2052f15
amoore33 <71346578+amoore33@users.noreply.github.com>
2020-09-15 14:38:52 -0400

    Add name prompt to script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var boxEle = document.body.querySelector(".box");
 
+var userName = window.prompt("What is your name?");
+
 var x1 = 5;
 var x2 = 3;
 
 boxEle.innerHTML = x1 + x2;
+boxEle.innerHTML += userName;
